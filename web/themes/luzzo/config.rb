4451cf56866ae51e12e4f8370e938dca1e344cb7
k9 <ggoncalovieira@gmail.com>
2018-02-11 02:13:55 +0000

    Initial configurations

--- web/themes/luzzo/config.rb
+++ web/themes/luzzo/config.rb
@@ -1,5 +1,16 @@
 
 
+# http_path = "/sites/all/themes/custom/bootstrap_mytheme"
+#css_dir = "css"
+#sass_dir = "assets/sass"
+#images_dir = "assets/images"
+#javascripts_dir = "js"
+#fonts_dir = "bootstrap/fonts/bootstrap"
+#generated_images_dir = "img"
+#http_images_path = http_path + "/" + generated_images_dir
+#http_generated_images_path = http_images_path
+#output_style = (environment == :production) ? :compressed : :expanded
+
 
 # We tell compass that we want to use. SCSS format in this case
 preferred_syntax = :scss
---
+++ web/themes/luzzo/js/main.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function(){
+
+  console.log("Bem Vindo ao website Luzzo 2018!");
+  console.log("by:gomastudio.net");
+
+
+});//end Doc
